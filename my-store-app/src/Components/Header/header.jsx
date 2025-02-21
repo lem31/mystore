70546a0a14394586ddccc7a552ce5458fb09540d
Leanne <lemeyer.no@gmail.com>
2025-02-21 09:50:26 +0100

    recreated app by removing files and started over, inital commit

---
+++ my-store-app/src/Components/Header/header.jsx
@@ -0,0 +1,33 @@
+function Header(){
+  return(
+   <header>
+
+    <p>
+      EasyBuy
+    </p>
+   <nav>
+    <ul>
+      <li >
+      </li>
+      <link to="/">Home</link>
+      <li>
+        <li>
+          <link to="/Sale">Sale</link>
+    
+        </li>
+        <li>
+          <link to="/About">About</link>
+        </li>
+        <li>
+          <link to="/Contact">Contact</link>
+        </li>
+
+      </li>
+
+      </ul>
+   </nav>
+   </header>
+  )
+}
+
+export default <Header/>
