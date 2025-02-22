78ebfcdb6d523cd5c271c787fc666a0ba15af5e1
Leanne <lemeyer.no@gmail.com>
2025-02-22 16:44:45 +0100

    Fixed search function so that suggestions are not displayed once an item is clicked

--- my-store-app/src/Components/Search/search.jsx
+++ my-store-app/src/Components/Search/search.jsx
@@ -12,37 +12,49 @@ const products = JSON.parse(localStorage.getItem("products")) || [];
 function Search(){
   const [searchPhrase, setSearchPhrase] = useState('');
   const [suggestions, setSuggestions] = useState([]);
+  const [showSuggestions, setShowSuggestions] = useState(true);
   const navigate = useNavigate();
 
   const handleChange = (e) => {
     const userInput = e.target.value;
     setSearchPhrase(userInput);
 
-    const filteredProducts = products.filter((product)=>product.title.toLowerCase().includes(userInput.toLowerCase()));
+    if (userInput){
+      const filteredProducts = products.filter((product)=>product.title.toLowerCase().includes(userInput.toLowerCase()));
     setSuggestions(filteredProducts);
+    setShowSuggestions(true);
+    } else{
+      setSuggestions([]);
+      setShowSuggestions(false);
+    }
+
+    
   };
   const handleSelect = (e, product)=> {
     e.preventDefault();
+
     navigate(`/product/${product.id}`);
+    setShowSuggestions(false);
+    setSearchPhrase
+    ('');
   };
 
   return(
     <div>
-    <form role="search">
-     <label htmlFor="search-input">Search</label>
-   
-     <img src={SearchIcon}/>
-     <input type="search" placeholder="What are you looking for?" aria-label="Search" value={searchPhrase} onChange={handleChange}/>
-     <button type="submit" aria-label="Submit search">Search</button>
-    </form>
-    {suggestions.length > 0 && (
-      <ul>
-        {suggestions.map((product)=>(
-          <li key={product.id} onClick={(e)=> handleSelect(e, product)}>{product.title}</li>
-        ))}
-      </ul>
-    )}
-       </div>
+      <form role="search">
+        <label htmlFor="search-input">Search</label>
+        <img src={SearchIcon} alt="Search Icon"/>
+        <input type="search" placeholder="What are you looking for?" aria-label="Search" value={searchPhrase} onChange={handleChange}/>
+        <button type="submit" aria-label="Submit search">Search</button>
+      </form>
+      {suggestions.length > 0 && (
+        <ul>
+          {suggestions.map((product)=>(
+            <li key={product.id} onClick={(e)=> { handleSelect(e, product); setSuggestions([]); }}>{product.title}</li>
+          ))}
+        </ul>
+      )}
+    </div>
   )
 };
 
