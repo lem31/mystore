bf9af72fd4e17da86caf8884fba90268b2541d71
Leanne <lemeyer.no@gmail.com>
2025-02-22 22:39:37 +0100

    Created Cart component

--- my-store-app/src/Components/Cart/Cart.jsx
+++ my-store-app/src/Components/Cart/Cart.jsx
@@ -1,7 +1,26 @@
-function Cart(){
-  return(
-    <img src="/my-store-app/src/Images/icons8-shopping-bag-50.png"></img>
-  )
-}
+import React from 'react';
+import useMyStore from '../Store/zustand_store';
+
+const Cart = () => {
+  const cart=useMyStore((state)=> state.cart);
+  const removeProductFromCart = useMyStore((state)=> state.removeProductFromCart);
+  return (
+    <div>
+      <h1>MY CART</h1>
+      {cart.length > 0 ?(
+        <ul>
+          {cart.map((product)=>(
+            <li key ={product.id}>
+              {product.title}
+              <button onClick={() => removeProductFromCart(product.id)}>Remove</button>
+            </li>
+          ))}
+        </ul>
+      ):(
+        <p> Your cart is empty.</p>
+      )}
+    </div>
+  );
+};
 
 export default Cart;
