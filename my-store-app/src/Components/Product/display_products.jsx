e8587c2a30937fb9be6416af7c5184c239ba7648
Leanne <lemeyer.no@gmail.com>
2025-02-23 14:33:39 +0100

    updated display products component with discounted price component

--- my-store-app/src/Components/Product/display_products.jsx
+++ my-store-app/src/Components/Product/display_products.jsx
@@ -1,19 +1,19 @@
 import ViewProductButton from "./view_product_button";
 import React from 'react';
 import useMyStore from '../Store/zustand_store'
+import DisplayDiscount from "./display_discount";
 
 const DisplayProducts = ({ product }) => {
-  const {price, discountedPrice} = product;
-  const discount = price > discountedPrice ? ((price - discountedPrice)/price) * 100 : 0;
+  const { discountedPrice} = product;
+
 return(
   <div>
   <h2>{product.title}</h2>
+  <DisplayDiscount product={product}/>
   <img src={product.image.url} alt={product.title} />
   <p>{product.description}</p>
   <p>${discountedPrice.toFixed(2)}</p>
-  {discount > 0 && (
-    <p> Discount: {discount.toFixed(2)}% off</p>
-  )}
+ 
   <ViewProductButton id={product.id} />
 </div>
 );};
