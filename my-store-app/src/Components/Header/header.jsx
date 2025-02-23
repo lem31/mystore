17b62e2394a71233170873efad03858c7fe52419
Leanne <lemeyer.no@gmail.com>
2025-02-23 23:18:19 +0100

    Added some styles to header

--- my-store-app/src/Components/Header/header.jsx
+++ my-store-app/src/Components/Header/header.jsx
@@ -1,16 +1,16 @@
 import CartIcon from "../Cart/cart_icon";
 import {Link} from "react-router-dom";
-
+import styles from "../../CSS_Modules/Header/header.module.css";
 
 function Header(){
   return(
     <div>
-   <header>
+   <header className={styles.header}>
 
-    <p>
+    <p className={styles.logo} >
       EasyBuy
     </p>
-   <nav>
+   <nav className={styles.nav}>
     <ul>
       <li >
       </li>
@@ -31,7 +31,10 @@ function Header(){
 
       </ul>
    </nav>
-<CartIcon/>
+   <div className={styles.shopping
+   }>
+<CartIcon />
+</div>
 
    </header>
    </div>
