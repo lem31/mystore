893fd4e4ba92b3f62b703e92bfddddb616ad4e2e
Leanne <lemeyer.no@gmail.com>
2025-02-23 09:56:22 +0100

    Updated store so that individual product count can also be displayed

--- my-store-app/src/Components/Store/zustand_store.jsx
+++ my-store-app/src/Components/Store/zustand_store.jsx
@@ -5,10 +5,27 @@ import {create} from 'zustand';
     products: [],
     cart: [],
     setProducts: (products) => set({  products }),
-    addProductToCart: (product) => set((state)=>({cart: [...state.cart, product]})),
-    removeProductFromCart: (productId) => set((state) => ({
-      cart: state.cart.filter((product)=> product.id !== productId)
-    })),
+    addProductToCart: (product) => set((state)=> {
+      const itemInCart = state.cart.find((item)=> item.id === product.id);
+      if (itemInCart){
+        return{
+          cart:state.cart.map((item)=> item.id === product.id ? {...item, quantity: item.quantity +1} : item),
+        };
+      }
+      return { cart: [...state.cart, {...product, quantity: 1}],};
+    }),
+    removeProductFromCart: (productId) => set((state) => {
+      const itemInCart = state.cart.find((item) => item.id === productId);
+      if(itemInCart && itemInCart.quantity > 1){
+        return{
+          cart: state.cart.map((item)=> item.id === productId ? { ...item, quantity: item.quantity -1} : item),
+        };
+        }
+        return {
+          cart: state.cart.filter((product)=> product.id !== productId),
+        };
+        }),
+      
     clearCart: () => set({cart:[]}),
     cartCount: (state)=> state.cart.length,
   }));
