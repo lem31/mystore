f7fd96d867694bead146cba42b7dc5cd7b3d10d3
Leanne <lemeyer.no@gmail.com>
2025-02-23 21:07:52 +0100

    Added a css module to style the suggestion list li

--- my-store-app/src/Components/Search/suggestion_list.jsx
+++ my-store-app/src/Components/Search/suggestion_list.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import styles from '../../CSS_Modules/Search_Suggestion_Li/Search_Suggestion_Li.module.css';
 
 function SuggestionList({suggestions, handleSelect}){
   return(
     <ul>
       {suggestions.map((product)=>(
-        <li key={product.id} onClick={(e)=> { handleSelect(e, product); }}>{product.title}</li>
+        <li key={product.id} className={styles.suggestion} onClick={(e)=> { handleSelect(e, product); }} >
+          {product.title}
+        </li>
       ))}
     </ul>
   )
