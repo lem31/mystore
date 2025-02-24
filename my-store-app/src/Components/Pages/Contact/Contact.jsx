3c2c35c81cba416f504790c9539bb8ecef130117
Leanne <lemeyer.no@gmail.com>
2025-02-24 22:25:41 +0100

    updated contacted page component and created a contact form component

--- my-store-app/src/Components/Pages/Contact/Contact.jsx
+++ my-store-app/src/Components/Pages/Contact/Contact.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import ContactForm from "../../Contact_Form/contact_form";
 
 function Contact(){
+  
   return(
     <div>
       <h1>Contact</h1>
+      <ContactForm/>
     </div>
   )
 }
