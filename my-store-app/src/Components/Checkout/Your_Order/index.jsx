ba99d021ed174433dd80ea279fd58031cb4a5850
Leanne <lemeyer.no@gmail.com>
2025-03-04 07:19:07 +0100

    updated checkout page styles

---
+++ my-store-app/src/Components/Checkout/Your_Order/index.jsx
@@ -0,0 +1,70 @@
+import React from 'react';
+import useMyStore from '../../Store';
+import DisplayProductSubtotal from '../Product_Subtotal';
+import styles from '../../../CSS_Modules/Cart_Page/cart.module.css';
+
+
+
+const YourOrder = () => {
+ 
+  const cart = useMyStore((state) => state.cart);
+  return (
+    <>
+
+    <div className={styles.outer_cart_box}>
+    
+<div className={styles.cart_card}>
+
+    <div className={styles.cart_card_titles} >
+      <p className={styles.product_card_title}>Product</p>
+
+      <p className={styles.subtotal_title}>Subtotal</p>
+    </div>
+      {cart.length > 0 ? (
+       
+        <ul >
+           
+          {cart.map((product) => (
+            product && (
+             
+              <li key={product.id} className={styles.product_items} >
+
+                <div className={styles.products_in_cart_div}>
+            
+                <div className={styles.image_div}>
+                <img className={styles.product_image} src={product.image.url} alt={product.title} />
+                </div>
+               
+               
+                <p className={styles.product_title}>{product.title}</p>
+              
+           
+                
+            
+    
+                <p className={styles.product_price}>${product.price}</p>
+             
+          
+               
+                <DisplayProductSubtotal product={product} />
+               
+                </div>
+              </li>
+        
+            )
+          ))}
+          
+        </ul>
+       
+      ) : (
+        <p>Your cart is empty.</p>
+      )}
+    </div>
+    </div>
+    </>
+  );
+};
+
+export default CartProductCard;
+
+
--- my-store-app/src/Components/Pages/Checkout_Page/index.jsx
+++ my-store-app/src/Components/Pages/Checkout_Page/index.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import PaymentForm from "../../../Components/Checkout/Payment_Form";
-
-
+import styles from '../../../CSS_Modules/Checkout_Page/checkout.module.css';
+import gStyles from '../../../CSS_Modules/Global_Styles/global_styles.module.css';
 function CheckoutPage() {
   return (
-    <div>
-      <h1>CHECKOUT</h1>
+    <div className={styles.container_checkout}>
+      <h1 className={gStyles.h1}>CHECKOUT</h1>
 <PaymentForm/>
     </div>
   );
