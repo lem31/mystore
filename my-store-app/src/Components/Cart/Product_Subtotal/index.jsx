ce44691381c973462f672c526f70b5c95666546e
Leanne <lemeyer.no@gmail.com>
2025-03-05 20:39:31 +0100

    added styles to checkout page

--- my-store-app/src/Components/Cart/Product_Subtotal/index.jsx
+++ my-store-app/src/Components/Cart/Product_Subtotal/index.jsx
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import useMyStore from '../../Store';
+import { useLocation } from 'react-router-dom';
 import styles from '../../../CSS_Modules/Cart_Page/cart.module.css';
-
+import checkoutStyles from '../../../CSS_Modules/Checkout_Page/checkout.module.css';
 
 
 
@@ -15,7 +16,7 @@ const DisplayProductSubtotal = ({ product }) => {
 
   return (
     <div>
-      <h2 className={styles.subtotal}> ${subtotal}</h2>
+      <h2 className={location.pathname=== '/MyCart'? styles.subtotal: checkoutStyles.checkout_subtotal}> ${subtotal}</h2>
     </div>
   );
 };
