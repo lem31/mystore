055d5111e65353a527201cd3854bc519f87ef605
Leanne <lemeyer.no@gmail.com>
2025-03-06 11:57:48 +0100

    added styles to checkout success page

--- my-store-app/src/Components/Pages/CheckoutSuccess_Page/index.jsx
+++ my-store-app/src/Components/Pages/CheckoutSuccess_Page/index.jsx
@@ -1,12 +1,24 @@
 import React from 'react';
+import checkoutSuccessStyles from '../../../CSS_Modules/Checkout_Success/checkout_success.module.css'
+import gStyles from '../../../CSS_Modules/Global_Styles/global_styles.module.css';
 
 function CheckoutSuccess(){
+  const orderNumber = Math.floor(Math.random() * 1000000);
+
+ 
+
   return(
-   <div>
-    <h1>
+<div className={checkoutSuccessStyles.container_checkout_success}>
+   <div className={checkoutSuccessStyles.div}>
+    <h1 className={gStyles.h1}>
       Thank you for your purchase!
     </h1>
+    <p className={checkoutSuccessStyles.p}>Your Order Number Is: {orderNumber}</p>
+    <p className={checkoutSuccessStyles.p}>We'll Email You An Order Confirmation With Details and Tracking Information</p>
+    <button className={gStyles.button}>CONTINUE SHOPPING</button>
     </div>
+    </div>
+
   )
 }
 
