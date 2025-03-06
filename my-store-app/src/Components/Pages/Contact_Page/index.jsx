036d065366c09efb90a4078ff3cf248e44619d06
Leanne <lemeyer.no@gmail.com>
2025-03-06 14:31:01 +0100

    updated contact page styles

--- my-store-app/src/Components/Pages/Contact_Page/index.jsx
+++ my-store-app/src/Components/Pages/Contact_Page/index.jsx
@@ -1,14 +1,39 @@
 import React from 'react';
 import ContactForm from "../../Contact_Form";
 import styles from '../../../CSS_Modules/Home_Page_Body/home_page_body.module.css';
+import gStyles from '../../../CSS_Modules/Global_Styles/global_styles.module.css';
+import contactStyles from '../../../CSS_Modules/Contact/contact.module.css';
+import locationImg from '../../../Images/icons8-location-50.png';
+import phoneImg from '../../../Images/icons8-phone-50.png';
+import emailImg from '../../../Images/icons8-email-50.png';
 
 function Contact(){
   
   return(
     <div className={styles.container}>
-      <h1>Contact</h1>
+    
+      <h1 className={gStyles.h1}>Contact US</h1>
+      <div className={contactStyles.layout_div}>
+     
+
+      <div>
+        <h2 className={gStyles.h2}>GET IN TOUCH</h2>
+        <p className={contactStyles.contact_p}>Just contact our 24h customer support team with any questions you may have
+          by email, phone or by sending us a message via our contact form, just press "Send".
+        </p>
+        <img src={locationImg} alt="location" />
+        <p className={contactStyles.contact_p}>ADDRESS <br /> 44 Chester Road, London, UK.</p>
+        <img src={phoneImg} alt="phone" />
+        <p className={contactStyles.contact_p}>PHONE <br /> 45367897</p>
+        <img src={emailImg} alt="email" />
+        <p className={contactStyles.contact_p}>EMAIL  <br />Eg@Example.com</p>
+      </div>
+
       <ContactForm/>
+
+      </div>
     </div>
+
   )
 }
 
