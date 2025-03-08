2cab7125d4e28f41736596969718cf21b78aa69e
Leanne <lemeyer.no@gmail.com>
2025-03-08 11:15:33 +0100

    fixed issue with discount location on product page

--- src/Components/Product/Display_Discount/index.jsx
+++ src/Components/Product/Display_Discount/index.jsx
@@ -1,16 +1,21 @@
 
 import React from 'react';
 import useMyStore from '../../Store'
+import { useLocation } from 'react-router-dom'
 import styles from '../../../CSS_Modules/Home_Page_Body/home_page_body.module.css';
+import productStyles from '../../../CSS_Modules/Product_Page/product_page.module.css'
 
 const DisplayDiscount = ({ product }) => {
   const {price, discountedPrice} = product;
   const discount = price > discountedPrice ? ((price - discountedPrice)/price) * 100 : 0;
+
+  const location= useLocation();
+  const isProductPage= location.pathname.includes('/product');
 return(
   <div className={styles.discount_div}>
   
   {discount > 0 && (
-    <p className={styles.product_discount}> - {Math.floor(discount)}% </p>
+    <p className={`${styles.product_discount} ${isProductPage ? productStyles.product_discount_mobile: ''}`}> - {Math.floor(discount)}% </p>
   )}
 
 </div>
