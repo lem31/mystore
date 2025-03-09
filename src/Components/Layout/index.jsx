2ba5120c2a09ec137802963c524fb96d92837cf2
Leanne <lemeyer.no@gmail.com>
2025-03-09 06:49:29 +0100

    fixed issue with bottom of product page

--- src/Components/Layout/index.jsx
+++ src/Components/Layout/index.jsx
@@ -3,18 +3,15 @@ import React from 'react';
 import {Outlet} from "react-router-dom";
 import Header from "../Header";
 import Footer from "../Footer";
-// import Search from "../Search/Search_Component/index";
+import styles from '../../CSS_Modules/Layout/layout.module.css';
 
 
 function Layout(){
 return(
-  <div >
+  <div className={styles.layout}>
    
   <Header/>
  
-
- 
-  
   <Outlet/>
   
   <Footer/>
